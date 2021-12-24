1b97edef6bb67b1f0a13da6ff73f42758103a0f4
shivamst11 <shivamtripathist11@gmail.com>
2021-12-24 10:59:40 +0530

    test2

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
+  let a = '';
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
